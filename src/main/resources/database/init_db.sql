76cf5977668cfc9194683bd39dd8c879f375bede
Andrii <andrii.syd@etiya.com>
2021-02-15 17:24:06 +0200

    Add sql scripts

---
+++ src/main/resources/database/init_db.sql
@@ -0,0 +1,36 @@
+drop database if exists queue;
+create database queue;
+\c queue
+
+create table participant
+(
+    id  serial primary key,
+    tag text not null
+);
+
+create table subject
+(
+    id      serial primary key,
+    name    text not null,
+    teacher text not null
+);
+
+create table schedule
+(
+    id         serial primary key,
+    subject_id int  not null,
+    day        text not null,
+    hour       time not null,
+    foreign key (subject_id) references subject (id)
+);
+
+create table queue
+(
+    id             serial primary key,
+    participant_id int  not null,
+    schedule_id    int  not null,
+    status         text not null,
+    enter_date     timestamp,
+    foreign key (participant_id) references participant (id),
+    foreign key (schedule_id) references schedule (id)
+);
