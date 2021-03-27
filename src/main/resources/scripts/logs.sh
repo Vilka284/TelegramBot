3bdaaa8c30d403305d0099d281da69fbc63ebf6f
andrii <viilka284@gmail.com>
2021-03-27 22:29:19 +0200

    Add empty end line

--- src/main/resources/scripts/logs.sh
+++ src/main/resources/scripts/logs.sh
@@ -1,4 +1,4 @@
 #  Reading services logs
 # Java Runtime exec does not support >, >>, |, & etc. shell operators so I used a bash
 
-journalctl -e -u bot.service > /home/pi/Bot/TelegramBot/src/main/resources/scripts/logs.txt
+journalctl -e -u bot.service >/home/pi/Bot/TelegramBot/src/main/resources/scripts/logs.txt
